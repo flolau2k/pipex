218d4012be7453f2400270148de9d8fd80bd78bc
flauer <flauer@student.42heilbronn.de>
2023-06-01 13:08:28 +0200

    split get_cmd and close all fds in forks

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 11:31:28 by flauer            #+#    #+#             */
-/*   Updated: 2023/06/01 10:03:17 by flauer           ###   ########.fr       */
+/*   Updated: 2023/06/01 13:05:58 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,16 @@ void free_splits(char **arr)
 	}
 	free(arr);
 	arr = NULL;
+}
+
+int	ft_arrlen(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr && arr[i])
+		++i;
+	return (i);
 }
 
 void	cleanup(t_pipex *st)
@@ -44,17 +54,21 @@ void	ft_error(t_pipex *st, ...)
 	char	*message;
 
 	va_start(args, st);
-	cleanup(st);
 	message = va_arg(args, char *);
-	while (message)
+	if (message)
 	{
 		write(STDERR_FILENO, message, ft_strlen(message));
-		write(STDERR_FILENO, ": ", 2);
 		message = va_arg(args, char *);
+		while (message)
+		{
+			write(STDERR_FILENO, ": ", 2);
+			write(STDERR_FILENO, message, ft_strlen(message));
+			message = va_arg(args, char *);
+		}
+		write(STDERR_FILENO, "\n", 1);
 	}
 	va_end(args);
-	if (errno)
-		perror(NULL);
+	cleanup(st);
 	exit(-1);
 }
 
@@ -82,7 +96,32 @@ char	**get_env(char *env[], char *key)
 	return (NULL);
 }
 
-char	*get_cmd(t_pipex *st, char *name, char *env[])
+char	*get_cmd_pwd(t_pipex *st, char *name, char *env[])
+{
+	char	**pwd;
+	char	*cmd;
+	char	*tname;
+
+	pwd = get_env(env, "PWD=");
+	if (ft_arrlen(pwd) != 1)
+	{
+		free_splits(pwd);
+		ft_error(st, "pipex: could not get pwd!");
+	}
+	tname = ft_strtrim(name, ".");
+	cmd = ft_strjoin(*pwd, tname);
+	free_splits(pwd);
+	free(tname);
+	if (access(cmd, X_OK))
+	{
+		perror(name);
+		free(cmd);
+		ft_error(st, NULL);
+	}
+	return (cmd);
+}
+
+char	*get_cmd_path(t_pipex *st, char *name, char *env[])
 {
 	char	**paths;
 	char	*path;
@@ -91,8 +130,6 @@ char	*get_cmd(t_pipex *st, char *name, char *env[])
 
 	i = 0;
 	cmd = NULL;
-	if (!name)
-		return (NULL);
 	paths = get_env(env, "PATH=");
 	while (paths[i])
 	{
@@ -111,19 +148,29 @@ char	*get_cmd(t_pipex *st, char *name, char *env[])
 	return (cmd);
 }
 
+char	*get_cmd(t_pipex *st, char *name, char *env[])
+{
+	if (!name)
+		return (NULL);
+	if (ft_strnstr(name, "./", 2) == name)
+		return (get_cmd_pwd(st, name, env));
+	else
+		return (get_cmd_path(st, name, env));
+}
+
 bool	init(t_pipex *st, char  *argv[], char *env[])
 {
 	if (access(argv[1], R_OK))
-		ft_error(st, argv[1], NULL);
+		perror(argv[1]);
 	if (!access(argv[4], F_OK) && access(argv[4], W_OK))
-		ft_error(st, argv[4], NULL);
+		perror(argv[4]);
 	st->fd1 = open(argv[1], O_RDONLY);
 	st->fd2 = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	pipe(st->pipe);
 	st->argv1 = ft_split(argv[2], ' ');
 	st->argv2 = ft_split(argv[3], ' ');
 	if (!st->argv1 || !st->argv2 || !st->argv1[0] || !st->argv2[0])
-		ft_error(st, "No command specified.", NULL);
+		ft_error(st, "pipex: No command specified.", NULL);
 	st->cmd1 = get_cmd(st, st->argv1[0], env);
 	st->cmd2 = get_cmd(st, st->argv2[0], env);
 	st->env = env;
@@ -132,20 +179,26 @@ bool	init(t_pipex *st, char  *argv[], char *env[])
 
 void	parent(t_pipex *st)
 {
-	close(st->pipe[0]);
-	dup2(st->pipe[1], STDOUT_FILENO); // redirect STDOUT to pipe input
+	dup2(st->pipe[1], STDOUT_FILENO); // redirect STDOUT to pipe input. close STDOUT, and dup st->pipe[1] to STDOUT
 	dup2(st->fd1, STDIN_FILENO); // redirect stdin to infile
+	close(st->pipe[0]);
+	close(st->pipe[1]);
+	close(st->fd1);
+	close(st->fd2);
 	execve(st->cmd1, st->argv1, st->env);
-	perror(strerror(errno));
+	perror("pipex");
 }
 
 void	child(t_pipex *st)
 {
-	close(st->pipe[1]);
 	dup2(st->pipe[0], STDIN_FILENO); // redirect STDIN to pipe output
 	dup2(st->fd2, STDOUT_FILENO);
+	close(st->pipe[0]);
+	close(st->pipe[1]);
+	close(st->fd1);
+	close(st->fd2);
 	execve(st->cmd2, st->argv2, st->env);
-	perror(strerror(errno));
+	perror("pipex");
 }
 
 void	px_init_null(t_pipex *st)
@@ -158,17 +211,14 @@ void	px_init_null(t_pipex *st)
 
 int	main(int argc, char *argv[], char *env[])
 {
-	// create a data structure: 
 	t_pipex	st;
 
 	px_init_null(&st);
-	// input to be expected:
-	// 4 arguments: infile cmd1 cmd2 outfile
 	if (argc != 5)
-		return (write(STDERR_FILENO, "Input Error\n", 12));
+		return (write(STDERR_FILENO, "Input Error: expected infile \"cmd1\" \"cmd2\" outfile\n", 51));
 	init(&st, argv, env);
-	
-	st.pid = fork();
+	if ((st.pid = fork()) == -1)
+		perror("pipex: fork");
 	if (st.pid)
 		parent(&st);
 	else
