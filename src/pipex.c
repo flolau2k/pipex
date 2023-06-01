5824f781bce523e832b038c6ff96388876c3f32b
flauer <flauer@student.42heilbronn.de>
2023-06-01 16:56:24 +0200

    wip

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 11:31:28 by flauer            #+#    #+#             */
-/*   Updated: 2023/06/01 13:05:58 by flauer           ###   ########.fr       */
+/*   Updated: 2023/06/01 16:39:17 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ void free_splits(char **arr)
 	arr = NULL;
 }
 
-int	ft_arrlen(char **arr)
-{
-	int	i;
+// int	ft_arrlen(char **arr)
+// {
+// 	int	i;
 
-	i = 0;
-	while (arr && arr[i])
-		++i;
-	return (i);
-}
+// 	i = 0;
+// 	while (arr && arr[i])
+// 		++i;
+// 	return (i);
+// }
 
 void	cleanup(t_pipex *st)
 {
@@ -48,29 +48,29 @@ void	cleanup(t_pipex *st)
 		free(st->cmd2);
 }
 
-void	ft_error(t_pipex *st, ...)
-{
-	va_list	args;
-	char	*message;
+// void	ft_error(t_pipex *st, ...)
+// {
+// 	va_list	args;
+// 	char	*message;
 
-	va_start(args, st);
-	message = va_arg(args, char *);
-	if (message)
-	{
-		write(STDERR_FILENO, message, ft_strlen(message));
-		message = va_arg(args, char *);
-		while (message)
-		{
-			write(STDERR_FILENO, ": ", 2);
-			write(STDERR_FILENO, message, ft_strlen(message));
-			message = va_arg(args, char *);
-		}
-		write(STDERR_FILENO, "\n", 1);
-	}
-	va_end(args);
-	cleanup(st);
-	exit(-1);
-}
+// 	va_start(args, st);
+// 	message = va_arg(args, char *);
+// 	if (message)
+// 	{
+// 		write(STDERR_FILENO, message, ft_strlen(message));
+// 		message = va_arg(args, char *);
+// 		while (message)
+// 		{
+// 			write(STDERR_FILENO, ": ", 2);
+// 			write(STDERR_FILENO, message, ft_strlen(message));
+// 			message = va_arg(args, char *);
+// 		}
+// 		write(STDERR_FILENO, "\n", 1);
+// 	}
+// 	va_end(args);
+// 	cleanup(st);
+// 	// exit(1);
+// }
 
 char	**get_env(char *env[], char *key)
 {
@@ -96,30 +96,30 @@ char	**get_env(char *env[], char *key)
 	return (NULL);
 }
 
-char	*get_cmd_pwd(t_pipex *st, char *name, char *env[])
-{
-	char	**pwd;
-	char	*cmd;
-	char	*tname;
+// char	*get_cmd_pwd(t_pipex *st, char *name, char *env[])
+// {
+// 	char	**pwd;
+// 	char	*cmd;
+// 	char	*tname;
 
-	pwd = get_env(env, "PWD=");
-	if (ft_arrlen(pwd) != 1)
-	{
-		free_splits(pwd);
-		ft_error(st, "pipex: could not get pwd!");
-	}
-	tname = ft_strtrim(name, ".");
-	cmd = ft_strjoin(*pwd, tname);
-	free_splits(pwd);
-	free(tname);
-	if (access(cmd, X_OK))
-	{
-		perror(name);
-		free(cmd);
-		ft_error(st, NULL);
-	}
-	return (cmd);
-}
+// 	pwd = get_env(env, "PWD=");
+// 	if (ft_arrlen(pwd) != 1)
+// 	{
+// 		free_splits(pwd);
+// 		ft_error(st, "pipex: could not get pwd!");
+// 	}
+// 	tname = ft_strtrim(name, ".");
+// 	cmd = ft_strjoin(*pwd, tname);
+// 	free_splits(pwd);
+// 	free(tname);
+// 	if (access(cmd, X_OK))
+// 	{
+// 		perror(name);
+// 		free(cmd);
+// 		ft_error(st, NULL);
+// 	}
+// 	return (cmd);
+// }
 
 char	*get_cmd_path(t_pipex *st, char *name, char *env[])
 {
@@ -144,21 +144,37 @@ char	*get_cmd_path(t_pipex *st, char *name, char *env[])
 	}
 	free_splits(paths);
 	if (!cmd)
-		ft_error(st, "pipex", name, "Command not found", NULL);
+		return (ft_strdup(name)); // ft_error(st, "pipex", name, "Command not found", NULL);
 	return (cmd);
 }
 
 char	*get_cmd(t_pipex *st, char *name, char *env[])
 {
+	char	*cmd;
+
+	cmd = NULL;
 	if (!name)
 		return (NULL);
-	if (ft_strnstr(name, "./", 2) == name)
-		return (get_cmd_pwd(st, name, env));
+	if (name[0] == '/' || ft_strnstr(name, "./", 2) == name)
+		return (ft_strdup(name));
+	// if (name[0] == '/')
+	// {
+	// 	cmd = ft_strdup(name);
+	// 	if (access(cmd, X_OK))
+	// 	{
+	// 		perror("pipex");
+	// 		free(cmd);
+	// 		ft_error(st, NULL);
+	// 	}
+	// 	return (cmd);
+	// }
+	// if (ft_strnstr(name, "./", 2) == name)
+	// 	return (get_cmd_pwd(st, name, env));
 	else
 		return (get_cmd_path(st, name, env));
 }
 
-bool	init(t_pipex *st, char  *argv[], char *env[])
+void	init(t_pipex *st, char  *argv[], char *env[])
 {
 	if (access(argv[1], R_OK))
 		perror(argv[1]);
@@ -169,12 +185,9 @@ bool	init(t_pipex *st, char  *argv[], char *env[])
 	pipe(st->pipe);
 	st->argv1 = ft_split(argv[2], ' ');
 	st->argv2 = ft_split(argv[3], ' ');
-	if (!st->argv1 || !st->argv2 || !st->argv1[0] || !st->argv2[0])
-		ft_error(st, "pipex: No command specified.", NULL);
 	st->cmd1 = get_cmd(st, st->argv1[0], env);
 	st->cmd2 = get_cmd(st, st->argv2[0], env);
 	st->env = env;
-	return (true);
 }
 
 void	parent(t_pipex *st)
@@ -185,6 +198,8 @@ void	parent(t_pipex *st)
 	close(st->pipe[1]);
 	close(st->fd1);
 	close(st->fd2);
+	if (!st->cmd1)
+		return ;
 	execve(st->cmd1, st->argv1, st->env);
 	perror("pipex");
 }
@@ -197,6 +212,8 @@ void	child(t_pipex *st)
 	close(st->pipe[1]);
 	close(st->fd1);
 	close(st->fd2);
+	if (!st->cmd2)
+		return ;
 	execve(st->cmd2, st->argv2, st->env);
 	perror("pipex");
 }
