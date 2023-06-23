823670ec34cda311d3cd7013c131a9b9fb93ebec
flauer <flauer@student.42heilbronn.de>
2023-06-23 17:53:53 +0200

    fix here_doc

--- bonus/here_doc_bonus.c
+++ bonus/here_doc_bonus.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 14:27:25 by flauer            #+#    #+#             */
-/*   Updated: 2023/06/23 14:54:24 by flauer           ###   ########.fr       */
+/*   Updated: 2023/06/23 17:51:08 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,19 @@ void	hd_child(int *pipe_fd, void *arg)
 	char	*line;
 	char	*lim;
 
-	lim = (char *)arg;
+	lim = ft_strjoin((char *)arg, "\n");
 	dup2(pipe_fd[1], STDOUT_FILENO);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
 	line = get_next_line(STDIN_FILENO);
-	while (ft_strnstr(line, lim, ft_strlen(lim)) != line)
+	while (ft_strncmp(line, lim, ft_strlen(lim)))
 	{
-		ft_printf("%s\n", line);
+		ft_printf("%s", line);
 		free(line);
 		line = get_next_line(STDIN_FILENO);
 	}
 	free(line);
+	free(lim);
 	exit(0);
 }
 
